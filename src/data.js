59655435b2a52924c80fa9fb0481cd7a6210d9f9
maxpres <maximilian.pres@web.de>
2022-02-26 12:59:00 +0100

    update

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ export const products = [
     id: 2,
     subject: 'To-do List',
     description:
-      'This to-do list is designed to make you life easier. By ticking boxes and using filters this tool is made to maintain an overview on you everyday tasks in clean and simple way.',
+      'This to-do list is designed to make you life easier. By ticking boxes and using filters this tool is made to maintain an overview on your everyday tasks in clean and simple way.',
     img: require('../src/img/todo.png'),
     tech: 'HTML, CSS, JavaScript',
     linknetlify: 'https://max-todolist.netlify.app',
