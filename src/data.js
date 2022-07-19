3343268c2b5e519a7837a23d63dde43b75a0b855
maxpres <maximilian.pres@web.de>
2022-07-19 20:31:37 +0200

    update projects

--- src/data.js
+++ src/data.js
@@ -1,6 +1,26 @@
 export const products = [
   {
     id: 1,
+    subject: 'House Marketplace',
+    description:
+      'Sell or Rent your house- This is a housemarket place, where people can register and sell or rent their home/appartment. You can create your own listing and choose between many settings.',
+    img: require('../src/img/immo.png'),
+    tech: 'HTML, CSS, JavaScript',
+    linknetlify: 'https://immoscout.vercel.app/',
+    linkgithub: 'https://github.com/maxpres/immoscout.git',
+  },
+  {
+    id: 2,
+    subject: 'Airbnb Clone',
+    description:
+      'This is an airbnb clone (UI) where you can search for accommodations in your favorite destinations. After choosing a date using the calender picker, you will get some proposals.',
+    img: require('../src/img/airbnb.png'),
+    tech: 'HTML, CSS, JavaScript',
+    linknetlify: 'https://airbnb-project-clone.netlify.app/',
+    linkgithub: 'https://github.com/maxpres/airbnb-clone.git',
+  },
+  {
+    id: 3,
     subject: 'Explore Bali',
     description:
       'On this website you can get a first idea of what it is like in Bali. Stunning beaches, rice terraces, centuries-old Hindu temples are only a fraction of what the island has to offer.',
@@ -10,17 +30,7 @@ export const products = [
     linkgithub: 'https://github.com/maxpres/explorebali-website',
   },
   {
-    id: 2,
-    subject: 'To-do List',
-    description:
-      'This to-do list is designed to make you life easier. By ticking boxes and using filters this tool is made to maintain an overview on your everyday tasks in clean and simple way.',
-    img: require('../src/img/todo.png'),
-    tech: 'HTML, CSS, JavaScript',
-    linknetlify: 'https://max-todolist.netlify.app',
-    linkgithub: 'https://github.com/maxpres/todo-list',
-  },
-  {
-    id: 3,
+    id: 4,
     subject: 'Quiz App',
     description:
       'Train your brain- A quiz created to screen your common sense. Your score and whether you could beat other participants, you will find out after you answered the last question. Good luck!',
@@ -30,13 +40,13 @@ export const products = [
     linkgithub: 'https://github.com/maxpres/quiz.github.io',
   },
   {
-    id: 4,
-    subject: 'Airbnb Clone',
+    id: 5,
+    subject: 'To-do List',
     description:
-      'Train your brain- A quiz created to screen your common sense. Your score and whether you could beat other participants, you will find out after you answered the last question. Good luck!',
-    img: require('../src/img/airbnb.png'),
+      'This to-do list is designed to make you life easier. By ticking boxes and using filters this tool is made to maintain an overview on your everyday tasks in clean and simple way.',
+    img: require('../src/img/todo.png'),
     tech: 'HTML, CSS, JavaScript',
-    linknetlify: 'https://airbnb-clone-mocha-ten.vercel.app/',
-    linkgithub: 'https://github.com/maxpres/airbnb-clone.git',
+    linknetlify: 'https://max-todolist.netlify.app',
+    linkgithub: 'https://github.com/maxpres/todo-list',
   },
 ];
